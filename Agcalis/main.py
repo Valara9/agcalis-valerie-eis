4a5e59eeb56bccd71cced9d57777a3e144bc057a
Val <valerieagcalis09@gmail.com>
2024-05-27 15:31:11 +0800

    hello love

---
+++ Agcalis/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect
+from users import Users
+from employees import Employees
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/check-user', methods=['POST'])
+def check_user():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    result = Users.check_user(username, password)
+
+    if result:
+        return redirect('/employee-list')
+    else:
+        return render_template("index.html")
+
+
+@app.route('/employee-list')
+def employee_list():
+    employees = Employees.get_all()
+    return render_template('employee.html' , employees=employees)
+    
+if __name__ == '__main__':
+    app.run()
---
+++ Agcalis/users.py
@@ -0,0 +1,16 @@
+from connector import Connector
+
+class Users:
+
+    def check_user(username , password):
+        query = "SELECT  * FROM users  WHERE username=%s AND password=%s"
+
+        Connector.cursor.execute(query, (username, password))
+        result = Connector.cursor.fetchone()
+
+        print(result)
+
+        if result is None:
+            return False
+
+        return True
